d60008e9f2d6b0e945fb2fcf3f781ac2a867e549
NishadhAniefa <86355135+NishadhAniefa@users.noreply.github.com>
2021-06-27 12:32:43 +0530

    Host project

---
+++ Company.Portal.HttpApi.Host/PortalBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace Company.Portal
+{
+    [Dependency(ReplaceServices = true)]
+    public class PortalBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "Portal";
+    }
+}
